2afe17670293c88d127b5bcad8963615e88da92f
Daniel <nifemiolaniyi4@gmail.com>
2024-08-08 03:02:06 +0100

    ---

--- script.js
+++
@@ -1,8 +0,0 @@
-const tags = document.getElementsByTagName("h1")
-console.log(tags)
-Array.from(tags).map(element => {
-	element.addEventListener("click", (e)=>{
-	console.log("Ran")
-	console.log(window.location.href)
-	});
-})
